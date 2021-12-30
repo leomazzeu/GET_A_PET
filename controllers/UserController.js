6bcb4bf9abfd36e5ec66d794771f5bb7fff13414
leomazzeu <leonardomazzeudasilva@gmail.com>
2021-12-30 10:35:00 -0300

    Função de login

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -66,6 +66,38 @@ module.exports = class UserController {
     } catch (error) {
       res.status(500).json({message: error}) // Lembrar de alterar!!!
     }
+  }
 
+  static async login(req, res) {
+
+    const { email, password } = req.body
+
+    if(!email) {
+      res.status(422).json({message: 'O email é obrigatório!'})
+      return
+    }
+
+    if(!password) {
+      res.status(422).json({message: 'A senha é obrigatória!'})
+      return
+    }
+
+    //check if user exists
+    const user = await User.findOne({ email })
+
+    if(!user) {
+      res.status(422).json({message: 'Não há um usuário cadastrado com este e-mail!'})
+      return
+    }
+
+    // check if password match with db password
+    const checkPassword = await bcrypt.compare(password, user.password)
+
+    if(!checkPassword) {
+      res.status(422).json({message: 'Senha incorreta!'})
+      return
+    }
+
+    await createUserToken(user, req, res)
   }
 }

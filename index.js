87f1bdd411746284f5835b0ca8d6f00657924755
leomazzeu <leonardomazzeudasilva@gmail.com>
2021-12-30 08:25:05 -0300

    Configurando index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors') // Verificar como o front interage sem o cors
+
+const app = express()
+
+// Config JSON Response
+app.use(express.json())
+
+// Solve CORS
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' })) //Permite que o back e o front se mantenham no mesmo endereço
+
+// Public folder for images
+app.use(express.static('public'))
+
+// Routes
+
+
+
+app.listen(5000, () => {
+  console.log('Servidor rodando na porta 5000!')
+})

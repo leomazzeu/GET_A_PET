0fbf20e6785d45847ae7cb89db1c2e6d76b68e38
leomazzeu <leonardomazzeudasilva@gmail.com>
2022-01-04 15:07:06 -0300

    Pegando pet por ID

--- controllers/PetController.js
+++ controllers/PetController.js
@@ -3,6 +3,7 @@ const Pet = require('../models/Pet')
 //helpers
 const getToken = require('../helpers/get-token')
 const getUserByToken = require('../helpers/get-user-by-token')
+const ObjectId = require('mongoose').Types.ObjectId
 
 module.exports = class PetController {
 
@@ -109,5 +110,23 @@ module.exports = class PetController {
     }
 
     res.status(200).json({ pets })
+  }
+
+  static async getPetById(req, res) {
+    const { id } = req.params
+
+    //check if id exists
+    if(!ObjectId.isValid(id)) {
+      return res.status(422).json({ message: "ID inválido!" })
+    }
+
+    //check if pet exists
+    const pet = await Pet.findById(id)
+
+    if(!pet) {
+      return res.status(404).json({ message: "Pet não encontrado!" })
+    }
+
+    res.status(200).json({ pet })
   }
 }
